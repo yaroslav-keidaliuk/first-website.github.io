861736ff1a89b006c295a95e0f439491616685a3
Yaroslav Keidaliuk <yaroslav.keidaliuk@gmail.com>
2024-07-13 22:39:57 +0300

    second commit

--- default.js
+++ default.js
@@ -1,3 +1,9 @@
 // Created on iPad.
 
 console.log("Hello, World!")
+
+const anotherPageButton = document.getElementById('another-page-button');
+
+anotherPageButton.addEventListener('click', ()=> {
+    window.location.href = 'page2.html';
+});
